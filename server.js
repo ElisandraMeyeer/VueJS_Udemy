01a286014e8a59de3761f12effac1ee888521888
ElisandraMeyeer <elisandra.meyer@alunos.sc.senac.br>
2022-08-01 10:19:17 -0300

    primeiro commit projeto

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static(__dirname + '/dist/'))
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/dist/index.html')
+})
+
+
+app.listen(3000, function(){
+    console.log('Servidor web rodando com Express')
+})
+
